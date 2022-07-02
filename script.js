e752bc139ce741389646660efd43e28989eb6fd9
Steve Calla <callasteven@gmail.com>
2022-07-02 14:42:35 -0600

    Prettier script js

--- script.js
+++ script.js
@@ -14,32 +14,40 @@ function writePassword() {
 // My code
 function generatePassword() {
   let passwordLength = getPasswordLength();
-  console.log('1', passwordLength)
   let validPasswordLength = validatePasswordLength(passwordLength);
-  console.log('2', validPasswordLength)
   let isCharacterType = validPasswordLength ? getCharacterTypes() : undefined;
-  console.log('3', isCharacterType)
-  let characterType = isCharacterType ? convertCharacterType(isCharacterType): undefined;
-  console.log('4', characterType)
-  let validCharacterTypes = characterType ? validateCharacterTypes(characterType) : undefined;
-  console.log('5', validCharacterTypes)
-  let characterTypeList = validCharacterTypes ? getCharacterTypeList(validPasswordLength, validCharacterTypes): undefined;
-  console.log('6', characterTypeList)
-  let password = characterTypeList ? createPassword(characterTypeList) : generatePassword();
-  console.log('7', password)
+  let characterType = isCharacterType
+    ? convertCharacterType(isCharacterType)
+    : undefined;
+  let validCharacterTypes = characterType
+    ? validateCharacterTypes(characterType)
+    : undefined;
+  let characterTypeList = validCharacterTypes
+    ? getCharacterTypeList(validPasswordLength, validCharacterTypes)
+    : undefined;
+  let password = characterTypeList
+    ? createPassword(characterTypeList)
+    : generatePassword();
   return password;
 }
 
 //get password length input
 function getPasswordLength() {
-  let numberOfCharacters = parseInt(window.prompt('How many characters would you like the password to contain?\n(must be at least 8 and at most 128)', 8));
+  let numberOfCharacters = parseInt(
+    window.prompt(
+      "How many characters would you like the password to contain?\n(must be at least 8 and at most 128)",
+      8
+    )
+  );
   return numberOfCharacters;
 }
 
 //ensure password length is a number (isNaN), is at least 8, is at most 128
 function validatePasswordLength(passwordLength) {
   if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    window.alert(`Password must be:\n\n  (a) at least 8 characters,\n  (b) at most 128 characters,\n  (c) a number.\n\n Please enter again.`);
+    window.alert(
+      `Password must be:\n\n  (a) at least 8 characters,\n  (b) at most 128 characters,\n  (c) a number.\n\n Please enter again.`
+    );
     // return generatePassword();
     return;
   } else {
@@ -51,24 +59,31 @@ function validatePasswordLength(passwordLength) {
 function getCharacterTypes() {
   let characterSelection = [];
 
-  let includeLowerCaseCharacters =  window.confirm(`LOWER CASE?\n\n(click OK to include & CANCEL to exclude)`);
+  let includeLowerCaseCharacters = window.confirm(
+    `LOWER CASE?\n\n(click OK to include & CANCEL to exclude)`
+  );
   characterSelection.push(includeLowerCaseCharacters);
-  
-  let includeUpperCaseCharacters =  window.confirm(`UPPER CASE?\n\n(click OK to include & CANCEL to exclude)`);
+
+  let includeUpperCaseCharacters = window.confirm(
+    `UPPER CASE?\n\n(click OK to include & CANCEL to exclude)`
+  );
   characterSelection.push(includeUpperCaseCharacters);
 
-  let includeSpecialCharacters =  window.confirm(`SPECIAL CHARACTERS?\n\n(click OK to include & CANCEL to exclude)`);
+  let includeSpecialCharacters = window.confirm(
+    `SPECIAL CHARACTERS?\n\n(click OK to include & CANCEL to exclude)`
+  );
   characterSelection.push(includeSpecialCharacters);
 
-  let includeNumericCharacters =  window.confirm(`NUMBERS?\n\n(click OK to include & CANCEL to exclude)`);
+  let includeNumericCharacters = window.confirm(
+    `NUMBERS?\n\n(click OK to include & CANCEL to exclude)`
+  );
   characterSelection.push(includeNumericCharacters);
 
   return characterSelection;
 }
 
 //convert character selection true/false to number lookup representing each character type/array
-function convertCharacterType(isCharacterType) { 
-  // console.log(isCharacterType);
+function convertCharacterType(isCharacterType) {
   let convertedCharacterSelection = [];
 
   for (let i = 0; i < isCharacterType.length; i++) {
@@ -76,27 +91,21 @@ function convertCharacterType(isCharacterType) {
       convertedCharacterSelection.push(i);
     }
   }
-
-  // console.log(convertedCharacterSelection);
   return convertedCharacterSelection;
 }
 
 //check that character type selected array length is greater than 0; if not request again
 function validateCharacterTypes(characterTypes) {
-  // if (!characterTypes.length > 0) {
+  let count = 0;
   if (characterTypes.length === 0) {
-    window.alert(`Password must contain at least one character type.\n\nPlease try again.`);
+    window.alert(
+      `Password must contain at least one character type.\n\nPlease try again.`
+    );
 
     let isCharacterType = getCharacterTypes();
-    console.log(isCharacterType);
     let characterType = convertCharacterType(isCharacterType);
-    console.log(characterType);
-    let validCharacterTypes = validateCharacterTypes(characterType);
-    console.log(validCharacterTypes);
-
-    return validCharacterTypes;
+    return validateCharacterTypes(characterType);
   }
-  // console.trace();
   return characterTypes;
 }
 
@@ -114,10 +123,100 @@ function getCharacterTypeList(numberOfCharacters, characterTypes) {
 }
 
 //creates password
-function createPassword(characterSelection) { //refactor
-  const alphabetLowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
-  const alphabetUpperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-  const specialCharacters = ['!','"','#','$','%','&','\'','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[','\\',']','^','_','`','{','|','}','~',']',';'];
+function createPassword(characterSelection) {
+  //refactor
+  const alphabetLowerCase = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const alphabetUpperCase = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const specialCharacters = [
+    "!",
+    '"',
+    "#",
+    "$",
+    "%",
+    "&",
+    "'",
+    "(",
+    ")",
+    "*",
+    "+",
+    ",",
+    "-",
+    ".",
+    "/",
+    ":",
+    ";",
+    "<",
+    "=",
+    ">",
+    "?",
+    "@",
+    "[",
+    "\\",
+    "]",
+    "^",
+    "_",
+    "`",
+    "{",
+    "|",
+    "}",
+    "~",
+    "]",
+    ";",
+  ];
   const numbers = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
 
   let password = [];
@@ -136,17 +235,14 @@ function createPassword(characterSelection) { //refactor
     } else if (characterSelection[i] === 3) {
       characters = numbers;
     } else {
-      console.log('something went wrong');
-      console.trace();
-      // return generatePassword();
-      return getCharacterTypes();
+      console.log("something went wrong");
     }
 
     //randomly select each character for password (from character arrays)
     randomNumber = getRandomNumber(characters.length);
     password.push(characters[randomNumber]);
   }
-  return password.join('');
+  return password.join("");
 }
 
 //generates random number as requested
@@ -154,4 +250,3 @@ function getRandomNumber(arrayLength) {
   let randomNumber = Math.floor(Math.random() * arrayLength);
   return randomNumber;
 }
-
